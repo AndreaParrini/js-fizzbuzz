40fbc73a060587a0b7155cdbd496a437eb9b05b6
AndreaParrini <andrea.parrini26@gmail.com>
2023-11-23 22:03:28 +0100

    create structure in js
    create file style.css

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
-console.log('ciao');
+const containerElement = document.querySelector('.container');
+
+console.log(containerElement);
+
+for( let i = 1; i <=100 ; i++){
+    const squareMarkup = `<div class="square"> ${i} </div>`;
+    console.log(squareMarkup);
+    containerElement.insertAdjacentHTML("beforeend", squareMarkup );
+}
