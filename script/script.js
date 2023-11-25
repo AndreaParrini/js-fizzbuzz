005460c04baac258906102b42ba90262972f548c
AndreaParrini <andrea.parrini26@gmail.com>
2023-11-25 10:31:10 +0100

    modify method to create div
    change method, now use append

--- script/script.js
+++ script/script.js
@@ -2,35 +2,42 @@ const containerElement = document.querySelector('.container');
 
 for (let i = 1; i <= 100; i++) {
 
-    /* prova creazione div con append
+    /* prova creazione div con append */
     const divElement = document.createElement('div');
-    document.querySelector('div').classList.add('bg-primary');
-    document.querySelector('div').classList.add('square');
-    divElement.append(i);
-    containerElement.append(divElement);  */
+    divElement.classList.add('square');
+    
 
-    let squareMarkup = "";
+    //let squareMarkup = "";
 
     if (i % 3 === 0 & i % 5 === 0) {
 
-        squareMarkup = `<div class="square bg-warning"> fizzbuzz </div>`;
+        divElement.classList.add('bg-warning');
+        divElement.append("FizzBuzz");
+        /* squareMarkup = `<div class="square bg-warning"> fizzbuzz </div>`; */
 
     } else if (i % 5 === 0) {
 
-        squareMarkup = `<div class="square bg-danger"> buzz </div>`;
+        divElement.classList.add('bg-danger');
+        divElement.append("Buzz");
+        /* squareMarkup = `<div class="square bg-danger"> buzz </div>`; */
 
     } else if (i % 3 === 0) {
 
-        squareMarkup = `<div class="square bg-secondary"> fizz </div>`;
+        divElement.classList.add('bg-secondary');
+        divElement.append("Fizz");
+        /* squareMarkup = `<div class="square bg-secondary"> fizz </div>`; */
 
     } else {
 
-        squareMarkup = `<div class="square bg-primary"> ${i} </div>`;
-        
+        divElement.classList.add('bg-primary');
+        divElement.append(i);
+        /* squareMarkup = `<div class="square bg-primary"> ${i} </div>`; */
 
     }
 
-    containerElement.insertAdjacentHTML("beforeend", squareMarkup);
+    containerElement.append(divElement);
+
+    /* containerElement.insertAdjacentHTML("beforeend", squareMarkup); */
 
 
 }
